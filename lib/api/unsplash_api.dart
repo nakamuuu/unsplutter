5eea43f067bd8a060ba7fd5f9d30b17a1997fcff
nakamuuu <keita111.mpgnt700.programming@gmail.com>
2018-05-04 00:05:20 +0900

    :art: Use collection operator instead of legacy `for` loops

--- lib/api/unsplash_api.dart
+++ lib/api/unsplash_api.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 import 'package:unsplutter/api/photo.dart';
 import 'package:unsplutter/local_variables.dart';
 
@@ -10,11 +10,8 @@ class UnsplashApi {
   static const headers = {'Authorization': "Client-ID ${LocalVariables.unsplashAccessKey}"};
 
   Future<List<Photo>> getPhotos({perPage: 30}) async {
-    final response = await http.get("$baseUrl/photos?per_page=$perPage", headers: headers);
-    final List<Photo> photos = [];
-    for (var json in json.decode(utf8.decode(response.bodyBytes))) {
-      photos.add(Photo.fromJson(json));
-    }
-    return photos;
+    final Response response = await get("$baseUrl/photos?per_page=$perPage", headers: headers);
+    final List decodedBody = json.decode(utf8.decode(response.bodyBytes));
+    return decodedBody.map<Photo>((json) => Photo.fromJson(json)).toList();
   }
 }
