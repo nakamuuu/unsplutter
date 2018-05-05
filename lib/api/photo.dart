69dabf702c4d65a18ffe6c281726c6761658e8c9
nakamuuu <keita111.mpgnt700.programming@gmail.com>
2018-05-05 21:28:15 +0900

    Split User model from photo.dart for use by other API

--- lib/api/photo.dart
+++ lib/api/photo.dart
@@ -1,3 +1,5 @@
+import 'package:unsplutter/api/user.dart';
+
 class Photo {
   final String id;
   final DateTime createdAt;
@@ -40,37 +42,5 @@ class Urls {
         json['regular'],
         json['small'],
         json['thumb'],
-      );
-}
-
-class User {
-  final String id;
-  final String username;
-  final String name;
-  final String bio;
-  final ProfileImage profileImage;
-
-  const User(this.id, this.username, this.name, this.bio, this.profileImage);
-
-  static fromJson(json) => User(
-        json['id'],
-        json['username'],
-        json['name'],
-        json['bio'],
-        ProfileImage.fromJson(json['profile_image']),
-      );
-}
-
-class ProfileImage {
-  final String small;
-  final String medium;
-  final String large;
-
-  const ProfileImage(this.small, this.medium, this.large);
-
-  static fromJson(json) => ProfileImage(
-        json['small'],
-        json['medium'],
-        json['large'],
       );
 }
