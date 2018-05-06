12ceaa90bc1bbdcf7e081c9675bd6ecef2fcde71
nakamuuu <keita111.mpgnt700.programming@gmail.com>
2018-05-06 20:47:22 +0900

    Adjust the layout of the drawer header

--- lib/ui/navigation_drawer.dart
+++ lib/ui/navigation_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
+import 'package:package_info/package_info.dart';
 import 'package:unsplutter/localizations.dart';
 import 'package:unsplutter/ui/home_content.dart';
 
@@ -18,14 +19,42 @@ class NavigationDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final List<Widget> drawerChildren = [
-      // TODO: Create formal header content
       new DrawerHeader(
-        child: new Text(
-          UnsplutterLocalizations.of(context).trans("app_name"),
-          style: Theme.of(context).textTheme.subhead.copyWith(color: Colors.white),
+        child: new Align(
+          alignment: Alignment.bottomLeft,
+          child: new Container(
+            height: 56.0,
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                new Row(children: [
+                  new Text(
+                    UnsplutterLocalizations.of(context).trans("app_name"),
+                    style: Theme.of(context).textTheme.body2.copyWith(color: Colors.white),
+                  ),
+                  new Text(
+                    UnsplutterLocalizations.of(context).trans("drawer_app_description"),
+                    style: Theme.of(context).textTheme.body1.copyWith(color: Colors.white70),
+                  ),
+                ]),
+                new Container(height: 2.0),
+                new FutureBuilder<String>(
+                  future: PackageInfo.fromPlatform().then((info) => "version ${info.version}"),
+                  builder: (context, snapshot) => new Text(
+                        snapshot.hasData ? snapshot.data : "",
+                        style: Theme.of(context).textTheme.body1.copyWith(color: Colors.white),
+                      ),
+                ),
+              ],
+            ),
+          ),
         ),
         decoration: new BoxDecoration(
-          color: Colors.black,
+          image: new DecorationImage(
+            image: new AssetImage('res/image/drawer_header.png'),
+            fit: BoxFit.cover,
+          ),
         ),
       ),
     ];
